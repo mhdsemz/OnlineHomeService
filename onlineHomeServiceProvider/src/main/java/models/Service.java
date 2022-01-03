acab7b0f1c091c4ebbc74bb3d243052f0170a762
mhdsemz <77237840+mhdsemz@users.noreply.github.com>
2022-01-03 15:31:23 +0330

    add prpperties tp service class

--- onlineHomeServiceProvider/src/main/java/models/Service.java
+++ onlineHomeServiceProvider/src/main/java/models/Service.java
@@ -1,2 +1,19 @@
-package models;public class Service {
+package models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+public class Service {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String description;
+    private double price;
+    @ManyToMany()
+    private List<Specialist> specialists=new ArrayList<>();
 }
--- onlineHomeServiceProvider/src/main/java/models/Specialist.java
+++ onlineHomeServiceProvider/src/main/java/models/Specialist.java
@@ -1,5 +1,17 @@
 package models;
 
-public class Specialist extends User {
+import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+public class Specialist extends User {
+    private byte[] image;
+    @ManyToMany(mappedBy = "specialists", fetch = FetchType.EAGER)
+    private List<Service> services = new ArrayList<>();
 }
