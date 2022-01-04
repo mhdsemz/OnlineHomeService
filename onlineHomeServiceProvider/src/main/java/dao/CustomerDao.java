679caa3bef90ab17a5b81b88266d95a5583ef5e9
mhdsemz <77237840+mhdsemz@users.noreply.github.com>
2022-01-04 23:34:30 +0330

    add save customer method in customerDao

--- onlineHomeServiceProvider/src/main/java/dao/CustomerDao.java
+++ onlineHomeServiceProvider/src/main/java/dao/CustomerDao.java
@@ -1,4 +1,20 @@
 package dao;
 
-public class CustomerDao {
+import models.Customer;
+import org.hibernate.Session;
+
+public class CustomerDao extends BaseDao {
+    private Session session;
+
+    public void save(Customer customer) {
+        if (customer == null) {
+            throw new RuntimeException("sorry customer is null!!!");
+        } else {
+            session = builderSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(customer);
+            session.getTransaction().commit();
+            session.close();
+        }
+    }
 }
