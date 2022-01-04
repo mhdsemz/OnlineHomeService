c14d8459bd1993c797d8aee7cb2af804469fa7ad
mhdsemz <77237840+mhdsemz@users.noreply.github.com>
2022-01-05 00:58:49 +0330

    add checkEmailValidation method in userValidation class

--- onlineHomeServiceProvider/src/main/java/validation/UserValidation.java
+++ onlineHomeServiceProvider/src/main/java/validation/UserValidation.java
@@ -1,13 +1,15 @@
 package validation;
 
+import exception.InvalidInformationException;
+
 public class UserValidation {
-//    public static String isValidEmail(String input) {
-//        if (input.matches("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
-//                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$"))
-//            return "true";
-//        else if (input.equals("0"))
-//            return "false";
-//
-//        throw new InValidUserInfoException("---- email that entered is not valid ----");
-//    }
+
+    public static String checkEmailValidation(String input) {
+        if (input.matches("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$"))
+            return "true";
+        else if (input.equals("0"))
+            return "false";
+        throw new InvalidInformationException("***check your email again!! something is wrong***");
+    }
 }
