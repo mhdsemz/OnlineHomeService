a1f40a0765f70f9950d1d87b8a11054b85613da2
mhdsemz <77237840+mhdsemz@users.noreply.github.com>
2022-01-04 23:25:01 +0330

    add buildersession factory to baseDao class

--- onlineHomeServiceProvider/src/main/java/dao/BaseDao.java
+++ onlineHomeServiceProvider/src/main/java/dao/BaseDao.java
@@ -1,4 +1,16 @@
 package dao;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class BaseDao {
+    public static SessionFactory sessionFactory;
+
+    public static SessionFactory builderSessionFactory() {
+        if (sessionFactory == null) {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+
 }
