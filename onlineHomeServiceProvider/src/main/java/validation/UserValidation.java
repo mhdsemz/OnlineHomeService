30acebeb50a3b96d87539e9361c09c80448fdeed
mhdsemz <77237840+mhdsemz@users.noreply.github.com>
2022-01-05 01:07:25 +0330

    add checkPhoneNumberValidation in userValidation class

--- onlineHomeServiceProvider/src/main/java/validation/UserValidation.java
+++ onlineHomeServiceProvider/src/main/java/validation/UserValidation.java
@@ -12,11 +12,18 @@ public class UserValidation {
             return "false";
         throw new InvalidInformationException("***check your email again!! something is wrong***");
     }
+
     public static String checkCharacterValidation(String input) {
         if (input.matches("^[a-zA-Z]+$"))
             return "true";
         else if (input.equals("0"))
             return "false";
         throw new InvalidInformationException("***please enter alphabet***");
+    }
+
+    public static boolean checkPhoneNumberValidation(String phoneNumber) {
+        if (phoneNumber.matches("^(\\+98|0)?9\\d{9}$"))
+            return true;
+        throw new InvalidInformationException("***your phoneNumber is not true!! please try again***");
     }
 }
