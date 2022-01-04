ed81d5405365c9d9e9bb4af4cca356a8b6725f56
mhdsemz <77237840+mhdsemz@users.noreply.github.com>
2022-01-05 00:01:31 +0330

    add save and getByPassAndPhoneNumber methodes to specialist class

--- onlineHomeServiceProvider/src/main/java/dao/SpecialistDao.java
+++ onlineHomeServiceProvider/src/main/java/dao/SpecialistDao.java
@@ -1,4 +1,37 @@
 package dao;
 
+import models.Customer;
+import models.Specialist;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import static dao.BaseDao.builderSessionFactory;
+
 public class SpecialistDao {
+    private Session session;
+
+    public void save(Customer customer) {
+        if (customer == null) {
+            throw new RuntimeException("sorry customer is null!!!");
+        } else {
+            session = builderSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(customer);
+            session.getTransaction().commit();
+            session.close();
+        }
+    }
+
+    public Specialist getSpecialistByPhoneNumberAndPass(String phoneNumber, String password) {
+        session = builderSessionFactory().openSession();
+        session.beginTransaction();
+        Query query = session.createQuery("FROM Specialist WHERE phoneNumber=:phoneNumber and password=:password");
+        query.setParameter("phoneNumber", phoneNumber);
+        query.setParameter("password", password);
+        Specialist specialist = (Specialist) query.getSingleResult();
+        session.getTransaction().commit();
+        session.close();
+        return specialist;
+    }
+
 }
