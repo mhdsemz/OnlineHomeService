c0b27395512ccd7e272a9b7901da78cccf15addc
mhdsemz <77237840+mhdsemz@users.noreply.github.com>
2022-01-05 00:12:52 +0330

    add save method in userDao class

--- onlineHomeServiceProvider/src/main/java/dao/OrderDao.java
+++ onlineHomeServiceProvider/src/main/java/dao/OrderDao.java
@@ -1,4 +1,24 @@
 package dao;
 
+import models.Manager;
+import models.Order;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import static dao.BaseDao.builderSessionFactory;
+
 public class OrderDao {
+    private Session session;
+
+    public void save(Order order) {
+        if (order == null) {
+            throw new RuntimeException("sorry order is null!!!");
+        } else {
+            session = builderSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(order);
+            session.getTransaction().commit();
+            session.close();
+        }
+    }
 }
--- onlineHomeServiceProvider/src/main/java/dao/UserDao.java
+++ onlineHomeServiceProvider/src/main/java/dao/UserDao.java
@@ -1,4 +1,23 @@
 package dao;
 
+import models.Manager;
+import models.User;
+import org.hibernate.Session;
+
+import static dao.BaseDao.builderSessionFactory;
+
 public class UserDao {
+    private Session session;
+
+    public void save(User user) {
+        if (user == null) {
+            throw new RuntimeException("sorry user is null!!!");
+        } else {
+            session = builderSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+            session.close();
+        }
+    }
 }
