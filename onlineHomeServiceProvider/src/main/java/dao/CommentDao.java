477b108d48739b9dc84510d04de88800394bb5c6
mhdsemz <77237840+mhdsemz@users.noreply.github.com>
2022-01-05 15:05:54 +0330

    add creat and delete methodes in suggestDao,commentDao classes

--- onlineHomeServiceProvider/src/main/java/dao/CommentDao.java
+++ onlineHomeServiceProvider/src/main/java/dao/CommentDao.java
@@ -1,5 +1,26 @@
 package dao;
 
-public class CommentDao {
+import models.Comment;
+import org.hibernate.Session;
+
+public class CommentDao extends BaseDao {
+    private Session session;
+
+    public void creatComment(Comment comment) {
+        session = builderSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(comment);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void deleteComment(Comment comment) {
+        session = builderSessionFactory().openSession();
+        session.beginTransaction();
+        session.delete(comment);
+        session.getTransaction().commit();
+        session.close();
+    }
+
 
 }
--- onlineHomeServiceProvider/src/main/java/dao/SuggestDao.java
+++ onlineHomeServiceProvider/src/main/java/dao/SuggestDao.java
@@ -1,4 +1,26 @@
 package dao;
 
-public class SuggestDao {
+import models.Comment;
+import models.Suggest;
+import org.hibernate.Session;
+
+public class SuggestDao extends BaseDao {
+    private Session session;
+
+    public void creat(Suggest suggest) {
+        session = builderSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(suggest);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void delete(Suggest suggest) {
+        session = builderSessionFactory().openSession();
+        session.beginTransaction();
+        session.delete(suggest);
+        session.getTransaction().commit();
+        session.close();
+    }
+
 }
