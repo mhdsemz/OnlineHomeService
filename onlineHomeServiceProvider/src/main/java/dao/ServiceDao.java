6a1949b806b3862e1a0dbbf743218a1e7f08b35a
mhdsemz <77237840+mhdsemz@users.noreply.github.com>
2022-01-05 15:12:14 +0330

    add update method in serviceDao class

--- onlineHomeServiceProvider/src/main/java/dao/ServiceDao.java
+++ onlineHomeServiceProvider/src/main/java/dao/ServiceDao.java
@@ -1,5 +1,34 @@
 package dao;
 
-public class ServiceDao {
+import models.Suggest;
+import models.service.Service;
+import org.hibernate.Session;
+
+public class ServiceDao extends BaseDao {
+    private Session session;
+
+    public void creat(Service service) {
+        session = builderSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(service);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void delete(Service service) {
+        session = builderSessionFactory().openSession();
+        session.beginTransaction();
+        session.delete(service);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void update(Service service) {
+        session = builderSessionFactory().openSession();
+        session.beginTransaction();
+        session.update(service);
+        session.getTransaction().commit();
+        session.close();
+    }
 
 }
