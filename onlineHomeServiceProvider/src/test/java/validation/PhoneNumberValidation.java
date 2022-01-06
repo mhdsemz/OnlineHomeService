52836b4f0012e612256d9b016f356897c68a8634
mhdsemz <77237840+mhdsemz@users.noreply.github.com>
2022-01-06 23:42:29 +0330

    add checphoneNumberValidation test method in PhoneNumberValidation class

---
+++ onlineHomeServiceProvider/src/test/java/validation/PhoneNumberValidation.java
@@ -0,0 +1,25 @@
+package validation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class PhoneNumberValidation {
+    UserValidation validation;
+
+    @BeforeEach
+    void init() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(UserValidation.class);
+        this.validation = (UserValidation) context.getBean("userValidation");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"09127297301", "09317297301"})
+    void givePhoneNumber_checkValidationNumbers(String phoneNumber) {
+        boolean b = validation.checkPhoneNumberValidation(phoneNumber);
+        Assertions.assertTrue(b);
+    }
+}
